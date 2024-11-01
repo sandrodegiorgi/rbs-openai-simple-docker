a1a0ef8a13c165ea2eea45b375bc4c880f542b34
Sandro Cosimo Degiorgi <degiorgi@rolf-benz-schule.de>
2024-11-01 15:32:12 +0100

    :/ need to automize separation between local and remote dev server

--- frontend/src/Consts.js
+++ frontend/src/Consts.js
@@ -1,5 +1,5 @@
-export const SERVER_URL = 'http://127.0.0.1:5000';
-// export const SERVER_URL = 'https://rbs-ai.degiorgi.de';
+// export const SERVER_URL = 'http://127.0.0.1:5000';
+export const SERVER_URL = 'https://rbs-ai.degiorgi.de';
 
 export const ASSISTANT_URL = "/api/assistants";
 export const INTERACT_EPILOGUE = "/interact";
