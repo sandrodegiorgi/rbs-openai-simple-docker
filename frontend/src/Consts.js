12d4ebc54a5302a92879da836b11e4ed13b752dc
Sandro Cosimo Degiorgi <degiorgi@rolf-benz-schule.de>
2025-01-19 18:21:17 +0100

    wip, but next release - time management

--- frontend/src/Consts.js
+++ frontend/src/Consts.js
@@ -1,7 +1,7 @@
 const packageJson = require('../package.json');
 
-export const SERVER_URL = 'http://127.0.0.1:5000';
-// export const SERVER_URL = 'https://rbs-ai.degiorgi.de';
+// export const SERVER_URL = 'http://127.0.0.1:5000';
+export const SERVER_URL = 'https://rbs-ai.degiorgi.de';
 
 export const ASSISTANT_URL = "/api/assistants";
 export const INTERACT_EPILOGUE = "/interact";
--- frontend/src/pages/ResponseDisplay/ResponseDisplay.js
+++ frontend/src/pages/ResponseDisplay/ResponseDisplay.js
@@ -14,6 +14,7 @@ import {
 import './ResponseDisplay.css';
 
 const ResponseDisplay = ({ response, string_headline }) => {
+
     const [copySuccess, setCopySuccess] = useState('');
     const [rawCopySuccess, setRawCopySuccess] = useState(false);
 
